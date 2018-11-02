21600ae585e84507fcd1c0ce9e95c62216f53742
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-01 23:00:09 -0400

    Added the constructors (just super calls) and overrided set, add, and void add. I need to do a sanity check to figure out how to sort this hold up

---
+++ OrderedArrayList.java
@@ -0,0 +1,27 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+  public OrderedArrayList() {
+    super();
+  }
+  public OrderedArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+
+  public T set(int index, T element) {
+    remove(index);
+    add(element);
+    return element;
+  }
+
+  public boolean add(T element) {
+    super.add(Sorted(element), element);
+    return true;
+  }
+
+  public void add(int index, T element) {
+    add(element);
+  }
+
+  /*SanityCheck for helper fxn to sort the array list
+  and figure what index to add to.*/
+  private int Sorted(T element) {}
+}
