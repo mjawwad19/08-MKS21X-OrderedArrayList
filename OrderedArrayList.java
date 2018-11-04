583b3a953a20868e13b2355ef7fc78d72d277e91
Mohammad Jawwad <mjawwad@stuy.edu>
2018-11-04 13:59:42 -0500

    I was rechecking my stuff and realized I never pushed these commits!

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,7 +7,8 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   public T set(int index, T element) {
-    remove(index);
+    if (element != null) remove(index);
+    //I didn't modify remove in NoNullArrayList sadly
     add(element);
     return element;
   }
@@ -22,9 +23,15 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   /*SanityCheck for helper fxn to sort the array list
-  and figure what index to add to.*/
+  and figure what index to add to.
+  - need to compare the given element to all other elements by looping through
+  - compareTo documentation states that if the element paramater is greater than the element
+    currently on the loop (via get(i)), it will be positive
+  - therefore you have to get to a point where the param is less than the element on the loop
+    but greater than the previous to find the index, therefore, index is incremented up*/
   private int Sorted(T element) {
     int i = 0;
+    if (element == null) return i;
     while (i < size() && element.compareTo(get(i)) > 0) {
       i++;
     }
